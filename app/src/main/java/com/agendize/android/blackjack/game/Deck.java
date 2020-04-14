762a110e3a93fecf8c405e7984366ff60e42319d
mel <l;k,dsjn>
2020-04-14 15:44:42 +0200

    Debut

--- app/src/main/java/com/agendize/android/blackjack/game/Deck.java
+++ app/src/main/java/com/agendize/android/blackjack/game/Deck.java
@@ -1,4 +1,20 @@
 package main.java.com.agendize.android.blackjack.game;
 
-public class Deck {
-}
+public void shuffleCards() {
+        Collections.shuffle(cards, new Random());
+        currentCard = 0; // Prochaine carte à prendre sera la première du tas
+        }
+public Card getNextCard() {
+        Card card = cards.get(currentCard);
+        currentCard++;  // 1 carte a été prise prendre la suivante la prochaine fois
+        return card;
+        }
+public Deck() {
+        cards = new ArrayList<>(52);
+        for (Card..Rank rank : Card.Rank.values()) {
+        for (Card.Suit suit : Card.Suit.values()) {
+        cards.add(new Card(rank, suit));
+        }
+        }
+        shuffleCards();
+        }
--- app/src/main/java/com/agendize/android/blackjack/game/Player.java
+++ app/src/main/java/com/agendize/android/blackjack/game/Player.java
@@ -1,4 +1,5 @@
 package main.java.com.agendize.android.blackjack.game;
 
 public class Player {
+var players = new
 }
