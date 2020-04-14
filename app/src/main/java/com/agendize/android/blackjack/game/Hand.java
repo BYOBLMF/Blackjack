92e8e799e36d3b8f89c8f7455f6a21f507e85f93
mel <l;k,dsjn>
2020-04-14 16:31:19 +0200

    J'ai fait ce que j'ai pu.

--- app/src/main/java/com/agendize/android/blackjack/game/Hand.java
+++ app/src/main/java/com/agendize/android/blackjack/game/Hand.java
@@ -1,4 +1,26 @@
 package main.java.com.agendize.android.blackjack.game;
 
+import com.agendize.android.blackjack.game.Card;
+
 public class Hand {
+    private Card[] mainjoueur;
+    private int nombrecartes;
+    public static final int Maximumdecartes = 6;
+
+    public maindujoueur(){
+        mainjoueur = new Card[Maximumdecartes]
+                nombrecartes = 0;
+    }
+    public void clear() {
+        nombrecartes = 0;
+        for(int i=0; i<Maximumdecartes;i++)
+            mainjoueur[i] = null;
+    }
+    public void addCard{
+        if (nombrecartes < Maximumdecartes){
+            mainjoueur[nombrecartes]<21;
+            nombrecartes++;
+        }
+    }
+
 }
